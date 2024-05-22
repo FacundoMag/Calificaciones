58de06f751073ab5882d841806206418226cef30
FacundoMag <facundo.maggeri.0608@gmail.com>
2024-05-21 21:09:21 -0300

    nueva modificación para valor de notas

--- src/componentes/ListaNotas.jsx
+++ src/componentes/ListaNotas.jsx
@@ -6,7 +6,10 @@ export default class ListaNotas extends Component {
       <div className="lista-notas">
         <ul>
           {this.props.notas.map((nota, index) => (
-            <li key={index}>
+            <li
+              key={index}
+              className={nota.nota >= 6 ? 'nota-alta' : 'nota-baja'}
+            >
               <span>{nota.asignatura}: </span>
               <input
                 type="number"
